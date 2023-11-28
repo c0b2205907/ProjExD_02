774661e3fe860e8380049f2eeb1932f7db352b13
C0B22059 <c0b2205907@edu.teu.ac.jp>
2023-11-28 15:10:12 +0900

    練習まで

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,8 +1,27 @@
 import sys
 import pygame as pg
-
+import random
 
 WIDTH, HEIGHT = 1600, 900
+delta ={
+    pg.K_UP:(0,-5),
+    pg.K_DOWN:(0,+5),
+    pg.K_LEFT:(-5,0),
+    pg.K_RIGHT:(+5,0)
+}
+
+def check_bound(rct: pg.Rect) ->tuple[bool,bool]:
+    """
+    オブジェクトが画面内or画面外を判断し、真理値タプルを返す関数
+    引数　  rctこんかとんor爆弾Surfaceのrect
+    戻り値：横方向、縦方向はみだし判定結果（画面内：True/画面外：False）
+    """
+    yoko,tate=True,True
+    if  rct.left < 0 or WIDTH < rct.right:
+        yoko=False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko,tate
 
 
 def main():
@@ -11,21 +30,51 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    bm = pg.Surface((10,10))
-    pg.draw.circle(bm,(255,0,0),(i,j)10)
-    bm.set_colorkey(())
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900,400
+    bb_img = pg.Surface((20,20))  #練習1　透明のSurfaceを作る
+    bb_img.set_colorkey((0,0,0))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+    bb_rct=bb_img.get_rect()  #練習2　爆弾
+    bb_rct.centerx = random.randint(0,WIDTH)
+    bb_rct.centery = random.randint(0,HEIGHT)
+
+    vx,vy=+5,+5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
+        if kk_rct.colliderect(bb_rct):
+            print("Game Over")
+            return
+
+        key_lst = pg.key.get_pressed()
+        sum_mv=[0,0]
+        for k,tpl in delta.items():
+            if key_lst[k]:  #キーが押されたら
+                sum_mv[0] += tpl[0]
+                sum_mv[1] += tpl[1]
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        kk_rct.move_ip(sum_mv[0],sum_mv[1])
+        if check_bound(kk_rct) !=(True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
+        screen.blit(kk_img,kk_rct)
+        bb_rct.move_ip(vx,vy)
+        yoko,tate = check_bound(bb_rct)
+        if not yoko:
+            vx *=-1
+        if not tate:
+            vy *=-1
+        bb_rct.move_ip(vx,vy)
+        screen.blit(bb_img,bb_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
